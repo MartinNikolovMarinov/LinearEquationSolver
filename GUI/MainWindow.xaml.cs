9ad971f165f2a39ddace4f6cec8794181b0a3c9e
martin <martin_n_marinov@mail.bg>
2021-09-30 22:53:29 +0300

    basic GUI support

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Windows;
 using System.Windows.Media.Imaging;
+using CSharpMath.SkiaSharp;
+using System.IO;
+using LinearEquationSolver.Parsers;
+using LinearEquationSolver;
+using System.Text;
 
 namespace GUI
 {
@@ -18,28 +22,90 @@ namespace GUI
         protected override void OnInitialized(EventArgs e)
         {
             base.OnInitialized(e);
-
-            //BitmapImage src = new BitmapImage();
-            //src.BeginInit();
-            //src.UriSource = new Uri("picture.jpg", UriKind.Relative);
-            //src.CacheOption = BitmapCacheOption.OnLoad;
-            //src.EndInit();
         }
 
-        private void BtnLoadFromFile_Click(object sender, RoutedEventArgs e)
+        private void BtnSolve_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            if (openFileDialog.ShowDialog() == true)
-            {
-                Uri fileUri = new Uri(openFileDialog.FileName);
-                imgDynamic.Source = new BitmapImage(fileUri);
-            }
-        }
+            string userInput = textBoxEquation.Text;
+            if (userInput == null) return;
 
-        //private void BtnLoadFromResource_Click(object sender, RoutedEventArgs e)
-        //{
-        //    Uri resourceUri = new Uri("/Images/white_bengal_tiger.jpg", UriKind.Relative);
-        //    imgDynamic.Source = new BitmapImage(resourceUri);
-        //}
+            //string userInput = @"
+            //    \begin{array}{rcc}
+            //            2x - 2y & = & 0 \\
+            //            z + 3w  & = & 2 \\
+            //            3x + 3y & = & 0 \\ 
+            //            x - y + 2z + 6w & = & 4
+            //    \end{array}
+
+            //    \quad \quad \quad \quad
+            //    \frac{1}{2}p_{2} + p_{3}
+            //    \longrightarrow
+            //    \quad
+
+            //    \begin{array}{rcc}
+            //            2x - 2y & = & 0 \\
+            //            z + 3w  & = & 2 \\
+            //            3x + 3y & = & 0 \\ 
+            //            x - y + 2z + 6w & = & 4
+            //    \end{array}
+
+            //    \quad \quad \quad \quad
+            //    \frac{1}{2}p_{2} + p_{3}
+            //    \longrightarrow
+            //    \quad
+
+            //    \begin{array}{rcc}
+            //            2x - 2y & = & 0 \\
+            //            z + 3w  & = & 2 \\
+            //            3x + 3y & = & 0 \\ 
+            //            x - y + 2z + 6w & = & 4
+            //    \end{array}
+
+            //    \quad \quad \quad \quad
+            //    \frac{1}{2}p_{2} + p_{3}
+            //    \longrightarrow
+            //    \quad
+
+            //    \begin{array}{rcc}
+            //            2x - 2y & = & 0 \\
+            //            z + 3w  & = & 2 \\
+            //            3x + 3y & = & 0 \\ 
+            //            x - y + 2z + 6w & = & 4
+            //    \end{array}
+            //";
+
+            var painter = new MathPainter();
+            ILinearEquationParser lep = new BasicLinearEquationParser();
+            string[] userSplit = userInput.Split(Environment.NewLine);
+            LinearSystem ls = new LinearSystem();
+            foreach (string rawEq in userSplit)
+            {
+                LinearEquation eq = lep.Parse(rawEq);
+                ls.AddEquation(eq);
+            }
+
+            ls.Solve();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (LinearEquation eq in ls.Equations)
+            {
+                sb.Append(eq.ToString());
+                sb.Append(@"\\");
+            }
+
+            painter.LaTeX = sb.ToString();
+            Stream pngStream = painter.DrawAsStream();
+            
+            var bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.StreamSource = pngStream;
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+            bitmap.EndInit();
+            bitmap.Freeze();
+
+            imgDynamic.Source = bitmap;
+            imgDynamic.Width = bitmap.Width;
+            imgDynamic.Height = bitmap.Height;
+        }
     }
 }
